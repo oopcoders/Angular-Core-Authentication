a99a84b72741728215ad0161bbbdbc18798a0a6f
Michal Harrison <mlh1477@gmail.com>
2020-09-14 12:18:37 -0400

    Video 2 Set up Identity in database context and run migration

--- Core/Data/ApplicationDBContext.cs
+++ Core/Data/ApplicationDBContext.cs
@@ -1,9 +1,10 @@
 using Core.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Core.Data
 {
-	public class ApplicationDBContext : DbContext
+	public class ApplicationDBContext : IdentityDbContext
 	{
 		public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
 		{
